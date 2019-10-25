4cfc248506e9017f8c2a59298b21166a155cf05c
Patricia <pat.fargetti@gmail.com>
2019-10-25 15:03:49 -0700

    changes in the css

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
-console.log("hello world");
+$(".btn").on("change", function(event) {
+  const selector = $(event.currentTarget)
+    .find(".selector")
+    .val();
+  event.preventDefault();
+
+  $.getJSON(
+    "https://api.nytimes.com/svc/topstories/v2/{section}.json?api-key=dhjSMeUrdWCaf0NTHIwDqpEMTZBa3QV4",
+
+    function(data) {
+      console.log(data);
+
+      console.log(selector);
+      $.each(data.section, function(index, value) {
+        console.log(value, index);
+      });
+    }
+  );
+});
