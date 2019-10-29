a5cc1c6741026216328be320e91c3fad0b88d899
Patricia <pat.fargetti@gmail.com>
2019-10-29 16:20:03 -0700

    adding loading gif

--- js/script.js
+++ js/script.js
@@ -6,30 +6,41 @@ document.addEventListener("DOMContentLoaded", function(event) {
       .toLowerCase();
     console.log(selector);
 
+    // diplay loading gif
+    $("#loading-gif").addClass("is-active");
+
     $.ajax({
       method: "GET",
       url: `https://api.nytimes.com/svc/topstories/v2/${selector}.json?api-key=dhjSMeUrdWCaf0NTHIwDqpEMTZBa3QV4`,
       dataType: "json"
-    }).done(function(nytArticles) {
-      console.log(nytArticles);
-      const results = nytArticles.results;
-      $("#story-list").empty();
+    })
+      .done(function(nytArticles) {
+        console.log(nytArticles);
+        const results = nytArticles.results.slice(0, 12);
+        $("#story-list").empty();
 
-      $.each(results, function(index, value) {
-        console.log(value);
+        $.each(results, function(index, value) {
+          console.log(value);
 
-        const abstract = value.abstract;
-        const img = value.multimedia[4].url;
+          const abstract = value.abstract;
+          const img = value.multimedia[4].url;
 
-        $("#story-list").append(
-          `
+          $("#story-list").append(
+            `
           <li class='stories' style='background-image:url(${img});'>
-              ${abstract}
+            <p class='story-text'>  ${abstract}</p>
            
           </li>
           `
-        );
-      }); // end of .each
-    }); // end of .done
+          );
+        }); // end of .each
+      })
+      .fail(function() {
+        console.log("something did not work?");
+      })
+      .always(function() {
+        // console.log("always has run");
+        $("#loading-gif").removeClass("is-active");
+      }); // end of .done
   });
 });
